44a6d12cdc32696593427c7ba5163bac0f8433c6
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-01-06 19:32:00 +0100

    Replace while loop with for loop in 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,7 +6,5 @@ def print_reversed_list_integer(my_list=[]):
     Parameters:
     - my_list: The list to be printed
     """
-    i = len(my_list) - 1
-    while i >= 0:
-        print("{:d}".format(my_list[i]))
-        i -= 1
+    for item in reversed(my_list):
+        print("{:d}".format(item))
