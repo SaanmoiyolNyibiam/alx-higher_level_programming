985752eed1aa9cf17b7c42c0d45e072b6a248ca1
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-01-06 17:47:32 +0100

    Update out of range checking mechanism in 2-eplace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -12,7 +12,7 @@ def replace_in_list(my_list, idx, element):
     - If idx is negative, return original list
     - If idx is out of range, return original list
     """
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
