20192110e11743b23bb7b7e9df4a7a7004745428
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-01-06 15:29:50 +0100

    Add a fuction that prints a list of integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+def print_list_integer(my_list=[]):
+    """
+    Prints each element of a list as an integer
+
+    Parameters:
+    - my_list (list): A list that contains integers
+    """
+    for item in my_list:
+        print("{:d}".format(item))
