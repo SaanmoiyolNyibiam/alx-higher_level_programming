f2022befefb6f012379493657a9b0cb75feac82c
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-01-06 17:38:58 +0100

    Add a function that replaces an element at a given index in a list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """
+    Replaces an element of a list at a specific position
+    Parameters:
+    - my_list: List to be mutated
+    - idx: Index of the element to replace
+    - element: The value to be used to replace
+
+    Return:
+    - If idx is negative, return original list
+    - If idx is out of range, return original list
+    """
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
