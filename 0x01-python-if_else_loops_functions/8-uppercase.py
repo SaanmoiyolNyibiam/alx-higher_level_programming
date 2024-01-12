eefa4f8d6acde365c81a16a6f91716c127bd5b38
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-01-12 15:18:18 +0100

    Update end value print format in 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,9 @@ def uppercase(str):
     Parameters:
     - str: String to be printed in uppercase
     """
+    i = 1
     for char in str:
         output = ord(char) - 32 if 97 <= ord(char) <= 122 else ord(char)
-        print("{:c}".format(output), end="")
-    print("{:c}".format(ord('\n')), end="")
+        end_val = "\n" if i == len(str) else ""
+        print("{:c}".format(output), end=end_val)
+        i += 1
