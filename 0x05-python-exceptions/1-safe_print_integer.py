ad85e85f30e192ecc5aea956c61284f33c4fbad7
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-01-24 11:51:18 +0100

    Add a funcion that prints an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    safe_print_integer - Prints an integer
+    value: The integer to be printed
+
+    Return: True if value has been correctly printed
+    else, false
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return false
