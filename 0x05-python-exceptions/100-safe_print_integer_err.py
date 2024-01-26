a80a3ab04c44c241e3c9af6fcca34cba6df3918f
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-01-26 14:21:29 +0100

    Update format for printing exception error in 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -17,5 +17,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError:
-        print("{}".format(err_msg), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
