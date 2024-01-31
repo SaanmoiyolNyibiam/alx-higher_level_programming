2fefc2cf1ca1ce7903eed40bc4967f8b688a22c1
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-01-31 22:13:23 +0100

    Add removed comment line back to 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+# 0-square.py by Valentine Nyibiam
 """A module for my classes"""
 
 
 class Square:
-    """This is a class that difines as square"""
+    """This is a class that defines a square"""
     pass
