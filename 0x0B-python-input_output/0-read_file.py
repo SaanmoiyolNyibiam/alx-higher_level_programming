8572db9d46565912ae543ac9a5cb3974a7f91371
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-02-06 15:58:54 +0100

    Remove format specifier from format method in 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,6 +11,6 @@ def read_file(filename=""):
     try:
         with open(filename, encoding="utf-8") as input_file:
             file_content = input_file.read()
-            print("{:s}".format(file_content))
+            print("{}".format(file_content))
     except FileNotFoundError:
         pass
