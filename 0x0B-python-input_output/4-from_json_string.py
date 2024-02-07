30a9d50ebebe0f3bf78d750024ac31597a498422
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-02-07 05:56:01 +0100

    Add a function that returns an object represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+""" This module defines a string to object function """
+
+
+def from_json_string(my_str):
+    """
+    from_json_string - Returns the object representation of a json string
+    Parameter:
+    my_obj: The string to be deserialized
+
+    Return:
+    An object(Python data structure)
+    """
+    return json.loads(my_str)
