9c6c63c9f14d4aa195888a7b5084779d1c203257
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-02-08 04:23:36 +0100

    Reduce function dcumentation line length to fit pycodestyle specifications

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,7 +5,8 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """
-    save_to_json_file - Writes an object to a text file, using a JSON representation
+    save_to_json_file - Writes an object to a text file,
+    using a JSON representation
     Parameter:
     - my_obj: The obj to be written to file
     - filename: The file which object will be written to
