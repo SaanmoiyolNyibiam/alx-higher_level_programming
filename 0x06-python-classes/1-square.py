7f5853ad383ef2c77c8f452bd998af8af931a864
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-02-09 17:39:54 +0100

    Add a class that defifnes a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module defines a defines a class square """
+
+
+class Square:
+    """ This is a class that defines a square """
+    def __init__(self, size):
+        self.__size = size
