d84795b16aa501d4684d25f1f7af95c83e2d9c59
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-02-10 05:38:44 +0100

    Add a property and setter method to the Square class in 4-squuare.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This module defines a class that defines a square """
+
+
+class Square:
+    """ This class defines a square """
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Returns the size of the square """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Sets the size of the square """
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Returns the area of the square """
+        return self.__size ** 2
