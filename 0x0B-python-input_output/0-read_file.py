b634163286ab472957386dbe7b87f7c5163701a6
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-02-12 15:52:35 +0100

    Change print format in 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ This is a module for an I/O function"""
-import sys
 
 
 def read_file(filename=""):
@@ -9,6 +8,5 @@ def read_file(filename=""):
     parameter:
     - filename: The file to read from
     """
-    with open(filename, "r") as input_file:
-        file_content = input_file.read()
-        print("{}".format(file_content))
+    with open(filename, encoding="utf-8") as input_file:
+        print(input_file.read())
