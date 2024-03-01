955ecbe1f0096dd66b9412e58f28801ed66e9418
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-03-01 16:34:49 +0100

    (Update 1-last_digit.py) Edit condition when number is less than 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,14 +6,8 @@ number = random.randint(-10000, 10000)
 if number < 0:
     number *= -1
     last_digit = number % 10
-    if last_digit > 5:
-        print("Last digit of -{} is -{} and is "
-              "greater than 5".format(number, last_digit))
-    if last_digit == 0:
-        print("Last digit of -{} is -{} and is 0".format(number, last_digit))
-    if last_digit < 6 and last_digit > 0:
-        print("Last digit of -{} is -{} and is "
-              "less than 6 and not 0".format(number, last_digit))
+    print("Last digit of -{} is -{} and is "
+          "less than 6 and not 0".format(number, last_digit))
 else:
     last_digit = number % 10
     if last_digit > 5:
@@ -21,6 +15,6 @@ else:
               "greater than 5".format(number, last_digit))
     if last_digit == 0:
         print("Last digit of {} is {} and is 0".format(number, last_digit))
-    if last_digit < 6 and last_digit > 0:
+    if last_digit < 6 and last_digit != 0:
         print("Last digit of {} is {} and is "
               "less than 6 and not 0".format(number, last_digit))
