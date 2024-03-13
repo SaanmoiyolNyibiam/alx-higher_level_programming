401259e24cff887acbed8636870cbfa609ed008f
ValentineNybiam <nyibiamsaanmoiyol@gmail.com>
2024-03-13 02:45:37 +0100

    feat(root): add a script that lists all rows of a table

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,7 @@
+-- Lists all rows of a table from a database in a MySQL server
+-- All fields should be printed
+-- The database name will be passed as an argument
+-- of the mysql command
+
+SELECT * FROM first_table;
+
