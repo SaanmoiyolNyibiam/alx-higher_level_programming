0fa7b310744c2a0bbfbb216656505ee1008ee163
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-04 16:30:06 +0100

    style(0x06-python-classes:5-square.py): remove checks in init method and properly set the size variable of the class using the setter

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,11 +5,12 @@
 class Square:
     """ This class defines a square """
     def __init__(self, size=0):
-        if isinstance(size, int) is False:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
+        # if isinstance(size, int) is False:
+        #     raise TypeError("size must be an integer")
+        # if size < 0:
+        #     raise ValueError("size must be >= 0")
+        # self.__size = size
 
     @property
     def size(self):
