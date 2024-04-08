1fb32611b091ff38c082c588eb52453749136378
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-08 21:31:49 +0100

    style(alx-hlp:0x12-javascript): update code style to be semistandard complaint

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-let input = parseInt(argv[2]);
+const input = parseInt(argv[2]);
 if (input) {
-	console.log('My number:', input);
-}
-else {
-	console.log('Not a number');
+  console.log('My number:', input);
+} else {
+  console.log('Not a number');
 }
