c66a4cdd5629ab71119a090558f026b438cc6c8e
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-09 22:14:13 +0100

    fix(alx-hlp:0x13-js): update file to import correct file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
 
   charPrint (printChar) {
     if (printChar) {
