579b8a2b277660d109436feaf0f58ae31106baad
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-08 18:49:55 -0700

    build(alx-hlp:0x12-javascript): add a script that searches for the second biggest integer in its list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,15 @@
 #!/usr/bin/node
-const { argv } = required('node:process');
+const { argv } = require('node:process');
 
-if (argv.length < 3) {
-	console.log(0)
+if (argv.length < 4) {
+  console.log(0);
+} else {
+  const intArray = [];
+  let i = 2;
+  while (argv[i]) {
+    intArray.push(parseInt(argv[i]));
+    i++;
+  }
+  intArray.sort((a, b) => b - a);
+  console.log(intArray[1]);
+}
