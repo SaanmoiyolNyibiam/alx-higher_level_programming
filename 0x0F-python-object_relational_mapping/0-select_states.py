aee51906013aad6b01a000171141ac8fb2f6d3f5
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-13 21:15:25 +0100

    build(alx-hlp:0x0F-python-object_relational_mapping): add a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Lists all the states from the database hbtn_0e_0_usa """
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    host = "localhost"
+    port = 3306
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    user = sys.argv[1]
+    db = MySQLdb.connect(host, user, passwd, db, port)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
