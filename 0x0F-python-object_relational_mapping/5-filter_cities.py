00e1264df24352853e708aa9abef4cb6a90716b8
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-15 15:14:39 +0100

    build(alx-hlp:0x0F-python-object_relational_mapping): add a script that takes in the name of a state as an argument and lists all cities of that state, using a database

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    Takes in the name of a state as an argument and lists
+    all the cities of that state from the database hbtn_0e_0_usa,
+    and is safe from MySQL injections.
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    host = "localhost"
+    port = 3306
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    user = sys.argv[1]
+    state_name = sys.argv[4]
+    db = MySQLdb.connect(host, user, passwd, db, port)
+    cur = db.cursor()
+    # use a parameterized query to prevent MySQL injection
+    cur.execute("SELECT cities.name FROM cities WHERE state_id=\
+                (SELECT id FROM states WHERE name = %s)", (state_name,))
+    output = cur.fetchall()
+    len = len(output)
+    for index, row in enumerate(output):
+        if (index + 1 == len):
+            print(row[0])
+        else:
+            print(row[0], end=", ")
+
+    cur.close()
+    db.close()
