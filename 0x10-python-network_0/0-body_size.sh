7916b6e0815795f07c13bc33d6d16abf83a3885f
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-25 15:38:59 +0100

    style(alx-hlp:0x10-python-network_0): make file three lines to meet project requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# this is a script that takes in a URL, sends a request to that
-# URL, and displays the size of the body of the response
-
-URL="$1"
-curl -s "$URL" | wc -m
+# sends a request to a URL and displays the size of the the body
+curl -s "$1" | wc -m
 
