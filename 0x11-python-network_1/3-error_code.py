2308957871ef26c0544da96089a86d45cdcd3087
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-27 16:34:22 +0100

    build(alx-hlp:0x11-python-network_1): add a script that sends a request to a URL and displays the body of the response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    this is a script that fetches a URL and displays the body
+    of the response, catching errors as well
+"""
+if __name__ == "__main__":
+    from urllib import request, error
+    from sys import argv
+
+    url = argv[1]
+    try:
+        with request.urlopen(url) as response:
+            data = response.read().decode('utf-8')
+    except error.HTTPError as err:
+        print(f"Error code: {err.code}")
