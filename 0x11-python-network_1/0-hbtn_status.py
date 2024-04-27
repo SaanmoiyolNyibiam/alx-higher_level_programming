4e4783e18baf9d61714d685613f2aa42b817c57d
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-27 09:50:34 +0100

    build(alx-hlp:0x11-python-network_1): add a script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# this is a script that fetches https://alx-intranet.hbtn.io/status
+if __name__ == "__main__":
+    import urllib.request
+
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(data)}")
+        print(f"\t- content: {data}")
+        print(f"\t- utf8 content: {data.decode('utf-8')}")
