81e67eae3673fde627ee6daf7752382bb52bdf40
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-27 19:44:19 +0100

    fix(alx-hlp:0x11-python-network_1): use the .Request method before using .urlopen

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,11 +9,10 @@ from sys import argv
 if __name__ == "__main__":
 
     url = argv[1]
+    req = request.Request(url)
     try:
-        with request.urlopen(url) as response:
+        with request.urlopen(req) as response:
             data = response.read().decode('utf-8')
             print(data)
     except error.HTTPError as err:
         print(f"Error code: {err.code}")
-    except error.URLError as err:
-        print("Error code: {err.reason}")
