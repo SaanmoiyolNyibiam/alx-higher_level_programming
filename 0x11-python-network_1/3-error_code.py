d66466f5ab093a05993b1d92110ab23705d356fe
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-27 19:23:50 +0100

    fix(alx-hlp:0x11-python-network_1): make file executable

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,9 +3,10 @@
     this is a script that fetches a URL and displays the body
     of the response, catching errors as well
 """
+from urllib import request, error
+from sys import argv
+
 if __name__ == "__main__":
-    from urllib import request, error
-    from sys import argv
 
     url = argv[1]
     try:
