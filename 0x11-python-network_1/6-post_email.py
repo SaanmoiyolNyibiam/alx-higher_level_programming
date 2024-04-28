6b387e99a3314c0032a32e06187e3ecabfcea89b
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-28 06:08:14 +0100

    build(alx-hlp:0x11-python-network_1): add a script that sends a POST request to a URL with an email parameter and displays the response body

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    this is a script that takes in a URL and and email,
+    sends a POST request to the URL with the email a parameter,
+    then displays the body of the response.
+"""
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    params = {'email': email}
+
+    data = post(url, params)
+    print(data.text)
