0b3779a49775025545b2a378b0853183ac80fd05
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-04-28 19:51:19 +0100

    fix(alx-hlp:0x11-python-network_1): adjust exception used

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -26,5 +26,6 @@ if __name__ == "__main__":
             print("No result")
         else:
             print(f"[{data.get('id')}] {data.get('name')}")
-    except exceptions.JSONDecodeError:
+    except exceptions.RequestException:
         print("Not a valid JSON")
+
