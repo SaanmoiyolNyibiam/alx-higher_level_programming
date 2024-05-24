19f9861f0dadeeef5c0445ba213cd15d6d56d4ff
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-05-24 18:16:29 +0100

    fix(alx-hlp-programming:0x14-javascript-web_scraping): update code algorithm

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /**
  * computes the number of tasks completed by user id
- * - he first argument is the API URL:
+ * - the first argument is the API URL:
  *   https://jsonplaceholder.typicode.com/todos
  * - only prints users with completed tasks
  * - you must use the module request
@@ -16,20 +16,14 @@ const URL = process.argv[2];
 
 request(URL, { json: true }, function (err, response) {
   if (err) {
-    console.log(err);
     process.exit(1);
   }
 
   const usersLog = {};
-  for (const user of response.body) {
-    usersLog[user.userId] = 0;
-  }
 
   for (const value of response.body) {
-    for (const user in usersLog) {
-      if (+user === value.userId && value.completed) {
-        usersLog[user] += 1;
-      }
+    if (value.completed) {
+      usersLog[value.userId] = (usersLog[value.userId] || 0) + 1;
     }
   }
   console.log(usersLog);
