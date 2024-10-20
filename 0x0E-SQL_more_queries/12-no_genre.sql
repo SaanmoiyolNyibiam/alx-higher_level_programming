e78d75fd9d6dd5c08b561240f56e0b89d6cc174e
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-20 20:06:01 +0100

    build(alx-hlp:0x0E): add solution to task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- This is an SQL script that lists all shows contained in h
+-- btn_0d_tvshows database without a genre linked
+
+SELECT title, tv_show_genres.genre_id
+FROM tv_shows 
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id 
+WHERE tv_show_genres.genre_id IS NULL;
+
