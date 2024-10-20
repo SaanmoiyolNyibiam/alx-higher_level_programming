b7353db7c357046cdef703b2e9cee3e953971c9a
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-20 14:21:16 +0100

    build(alx-hlp:0x0E): add solution to task 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- This is an SQL script that lists all cities contained
+-- in the database hbtn_0d_usa
+
+SELECT cities.id, cities.name, state.name
+FROM cities
+JOIN states ON cities.state_id = states.id;
+
