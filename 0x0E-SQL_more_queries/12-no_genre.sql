d07012ea566db2bc366ab29f7d61c22720ea7e6d
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-20 20:09:05 +0100

    refactor(alx-hlp:0x0E): addd an alias for column in query output

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- This is an SQL script that lists all shows contained in h
 -- btn_0d_tvshows database without a genre linked
 
-SELECT title, tv_show_genres.genre_id
+SELECT title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows 
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id 
 WHERE tv_show_genres.genre_id IS NULL;
