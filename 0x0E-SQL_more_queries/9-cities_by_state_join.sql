c097985712fa5a868f93695c908c2a35725e827e
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-20 14:35:08 +0100

    refactore(alx-hlp:0x0E): add alias for query result columns

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- This is an SQL script that lists all cities contained
 -- in the database hbtn_0d_usa
 
-SELECT cities.id, cities.name, state.name
+SELECT cities.id AS id, cities.name AS name, state.name AS name
 FROM cities
 JOIN states ON cities.state_id = states.id;
 
