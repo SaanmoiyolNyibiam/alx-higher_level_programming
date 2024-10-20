6fa506e883fc0a759ed2db1133c456b32cec51fe
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-20 20:14:16 +0100

    perf(alx-hlp:0x0E): add 'order by' clause to query to sort query result

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,5 +4,6 @@
 SELECT title, tv_show_genres.genre_id AS genre_id
 FROM tv_shows 
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id 
-WHERE tv_show_genres.genre_id IS NULL;
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
 
