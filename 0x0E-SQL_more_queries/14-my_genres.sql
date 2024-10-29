c359877a3adaa31750039fe9254a4bd6e601b346
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-29 16:40:36 +0100

    perf(alx-b-storage:0x02): fine tune query to sort results in ascending order

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,9 +2,10 @@
 -- database to lists all genres of the show Dexter
 
 
-SELECT tv_genres.name
+SELECT tv_genres.name AS name
 FROM tv_show_genres
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_shows.title = 'Dexter';
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name ASC;
 
