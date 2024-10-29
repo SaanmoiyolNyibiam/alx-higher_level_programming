08177ed29a44a242c6986bc08d3ef51bad0f2af6
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-29 15:11:44 +0100

    refactor(alx-b-storage:0x02): fine tune query to return a sorted result

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,9 @@
 -- This is an SQL script that lists genres from
 -- hbtn_0d_tvshows and displays the number of shows linked to each
 
-SELECT name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_genres.id
-GROUP BY genre;
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY name
+ORDER BY number_of_shows DESC;
 
