652e92317c143b2c9db327d2fec3085a236be122
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2025-08-07 14:30:34 +0100

    build(alx-hlp:0x0A): add solution and test file for task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" This is a module that defines a function """
+
+def is_kind_of_class(obj, a_class):
+    """ This is a function that checks the instance of an object """
+    return isinstance(obj, a_class)
