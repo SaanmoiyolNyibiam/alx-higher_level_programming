a5c00c121d84699836ca3f5d2c3133642045c2b3
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2025-08-07 15:29:28 +0100

    build(alx-hlp:0x0A): add solution and task file for task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This is a module that defines an empty class """
+
+class BaseGeometry():
+    """ This is an empty python class """
+    def area(self):
+        """
+        raises an Exception with the 
+        message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
