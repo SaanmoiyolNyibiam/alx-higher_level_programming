4e16bde2666473fd57caad9b56e37ed1177ebda6
Saanmoiyol Nyibiam <nyibiamsaanmoiyol@gmail.com>
2025-08-07 14:19:14 +0100

    build(alx-hlp:0x0A): add solution and test file for task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" This is a method that defines a function """
+
+def is_same_class(obj, a_class):
+    """ This is a function that checks the instance of a class """
+    return type(obj) is a_class
